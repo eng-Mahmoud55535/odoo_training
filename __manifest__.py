d988f9a35708aa6076f11884fa383c53d716a05a
mahmoud <mahmoudawdawd64@gmail.com>
2025-10-04 11:11:40 +0300

    Initial commit: Add Custom CRM module with Next Call Date field

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom CRM',
+    'version': '1.0',
+    'summary': 'Custom CRM module with Next Call Date field',
+    'description': 'Extends CRM Lead with a new field: Next Call Date',
+    'author': 'Mahmoud',
+    'depends': ['crm'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
+
